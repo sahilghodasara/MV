57b35209cdab33485644434cb4023d000801a51f
sahilghodasara <sahilghodasara@gmail.com>
2022-05-19 12:42:15 +0530

    added changes

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+
+    jQuery('.filter-btn').click(function(){
+        jQuery('.pop-box-filter').toggle();
+    });
+
+    jQuery('.search-mobile-icon a').click(function(){
+        jQuery('.search-box').toggle();
+    });
+
+});
